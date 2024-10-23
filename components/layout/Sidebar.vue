0b10982041d9fc8b79a049323681e595f687439f
unknown <mihail.diweew@yandex.ru>
2024-10-23 11:24:39 +0300

    tailwind & colortheme setup

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -4,6 +4,7 @@
             <NuxtImg src="/logo.svg" alt="" width="100px" class="mx-auto" />
         </NuxtLink>
         
+        <UiButton class="w-full mb-4 rounded-lg" variant="default">hello</UiButton>
     </aside>
 </template>
 
